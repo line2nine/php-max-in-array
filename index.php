27bb41b9f0428e311fe976c7cd6c3f0159978cda
line2nine <hailinh203@gmail.com>
2020-04-06 15:26:01 +0700

    Add files

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$Array = [
+    [3, 9, 43, 122, 64, 200],
+    [33, 4, 105, 644, 124],
+    [8, 51, 21, 555]
+];
+
+
+function findMax($Array)
+{
+    $index1 = 0;
+    $index2 = 0;
+    $max = $Array[0][0];
+    for ($i = 0; $i < count($Array); $i++) {
+        for ($j = 0; $j < count($Array[$i]); $j++) {
+            if ($Array[$i][$j] > $max) {
+                $max = $Array[$i][$j];
+                $index1 = $i;
+                $index2 = $j;
+            }
+        }
+    }
+    return $max . " ở vị trí " . "[" . $index1 . "]" . "[" . $index2 . "]";
+}
+
+echo "<h1>" . findMax($Array) . "</h1>";
